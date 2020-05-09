b1156c8be2b3ca80b0eba92df185c53903780e0a
cicimen <egemengulkilik@gmail.com>
2020-05-10 02:19:49 +0300

    EMAppDelegate add

---
+++ Euro-IOS/Classes/UIApplicationDelegate+EM.h
@@ -0,0 +1,18 @@
+//
+//  UIApplicationDelegate+EM.h
+//  Euro-IOS
+//
+//  Created by Egemen on 8.05.2020.
+//
+
+#ifndef UIApplicationDelegate_EM_h
+#define UIApplicationDelegate_EM_h
+
+@interface EMAppDelegate : NSObject
+
++ (void)sizzlePreiOS10MethodsPhase1;
++ (void)sizzlePreiOS10MethodsPhase2;
+
+@end
+
+#endif /* UIApplicationDelegate_EM_h */
