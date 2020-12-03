0cbaa351566f6726fabe7c9d7196151a97295c7e
cicimen <egemengulkilik@gmail.com>
2020-12-03 18:19:26 +0300

    sdk version upgrade to 1.9.12

--- Euro-IOS/Classes/EuroManager.h
+++ Euro-IOS/Classes/EuroManager.h
@@ -14,7 +14,7 @@
 #import "EMMessage.h"
 #import "EMTools.h"
 
-#define SDK_VERSION @"1.9.11"
+#define SDK_VERSION @"1.9.12"
 
 
 @interface EuroManager : NSObject  <UNUserNotificationCenterDelegate>
